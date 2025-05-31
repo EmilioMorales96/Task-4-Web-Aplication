4b1b104e533b77fd1511c70ad4d5240bffd987ad
Emilio Morales <163681468+EmilioMorales96@users.noreply.github.com>
2025-05-31 13:12:48 -0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+ host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }, // ¡Esto es obligatorio en Railway!
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 connection.connect((err) => {
