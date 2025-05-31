1a628f3d9e2c78e76b82b0860e03cee79bff28ec
Emilio Morales <163681468+EmilioMorales96@users.noreply.github.com>
2025-05-31 00:02:42 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,7 @@ if (!process.env.JWT_SECRET || !process.env.DB_HOST) {
 }
 
 // Middleware
-console.log('Starting backend...');
 console.log('Allowed CORS origin:', process.env.CLIENT_URL);
-console.log('DB Host:', process.env.DB_HOST);
 
 // Explicitly handle preflight requests for all routes
 app.options('*', cors({
@@ -71,5 +69,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
 });
+
 
 
